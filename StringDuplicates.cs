4c23b81360406559bb5cb0233b714480577e3049
Alex Berthon <Alexjberthon@gmail.com>
2022-01-25 10:21:48 -0800

    Added addittional logic for a better solution to the problem. Words like "mississippi" that repeat the same letter many times will now only output the repeated letter once instead of printing the repeated letter every time it was repeated. The added logic does not decrease the time efficiency or use any additional resources.

--- StringDuplicates.cs
+++ StringDuplicates.cs
@@ -3,16 +3,6 @@ using System.Linq;
 
 /**
 Practice Problem: "Print duplicate letters in a string"
-
-Assumptions: print letters that were repeated.
-             no addittional instructions on how to format output
-                 only list repeated letters once?
-                 count how many times a letter was repeated? 
-
-basic solution
-words that repeat letters more than twice get weird
-"hehehe" or "mississippi" for example
-can adapt solution to account for these as needed in part 2:
  */
 namespace Practice {
     public class StringDuplicates {
@@ -23,8 +13,8 @@ namespace Practice {
 
             for (int i = 0; i < input.Length; i++) {
                 for (int j = i + 1; j < input.Length; j++) {
-                    if (input.ElementAt(i) == input.ElementAt(j)) {
-                        output += input.ElementAt(i) + " ";
+                    if (input.ElementAt(i) == input.ElementAt(j) && !output.Contains(input.ElementAt(i))) {
+                            output += input.ElementAt(i) + " ";
                     }
                 }
             }
@@ -37,6 +27,6 @@ namespace Practice {
 /*
 static void Main(string[] args) {
             StringDuplicates test = new StringDuplicates();
-            test.helperFunction("veggie"); //replace input string to whatever string you wish to test
+            test.helperFunction("mississippi"); //replace input string to whatever string you wish to test
         }
 */
