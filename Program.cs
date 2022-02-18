bd3cff24bbf5dbde4c6966020cf7e861fafd330d
Alex Berthon <Alexjberthon@gmail.com>
2022-02-18 14:29:25 -0800

    Case statement testing

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Practice {
     internal class Program {
         static void Main(string[] args) {
-            StringProblems test = new StringProblems();
-            test.recursiveReverse("dog");
         }
     }
 }
-
