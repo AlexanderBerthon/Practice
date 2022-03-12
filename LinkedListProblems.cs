9bdfbac3c9f555869729744daf897abe850287b2
Alex Berthon <Alexjberthon@gmail.com>
2022-03-11 16:36:08 -0800

    LinkedList Problem 1 Solving a daily programming problem for practice.

---
+++ LinkedListProblems.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice {
+    public class LinkedListProblems {
+        public LinkedListProblems() { }
+
+        ///problem: rotate a singly linked list n times
+        public void rotate(int n) {
+            LinkedList<int> test = new LinkedList<int>();
+
+            //initialize
+            for (int i = 1; i < 6; i++) {
+                test.AddLast(i);
+            }
+
+
+            //print unedited list
+            Console.WriteLine();
+            foreach (int i in test) {
+                Console.Write(i+" ");
+            }
+
+            //rotate 'n' number of times
+            int temp = 0;
+            for (int i = n; i > 0; i--) {
+                temp = test.Last();
+                test.RemoveLast();
+                test.AddFirst(temp);
+            }
+
+            //print edited list
+            Console.WriteLine();
+            foreach (int j in test) {
+                Console.Write(j+" ");
+            }
+            Console.WriteLine();
+
+        }
+
+    }
+}
