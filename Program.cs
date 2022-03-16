2fa485e674de0a284bcc223ca5193db9ced8a710
Alex Berthon <Alexjberthon@gmail.com>
2022-03-16 15:19:55 -0700

    ArrayJump Problem Solved

--- Program.cs
+++ Program.cs
@@ -9,8 +9,18 @@ using System.Threading.Tasks;
 namespace Practice {
     internal class Program {
         static void Main(string[] args) {
-            LinkedListProblems demo = new LinkedListProblems();
-            demo.rotate(4);
+            ArrayProblems demo = new ArrayProblems();
+
+            //2, 3, 0, 3, 1
+            //2, 1, 2, 1, 1
+            //5, 4, 3, 4, 1, 1, 1, 1, 1, 1, 1, 1 
+            int[] input = { 2, 3, 0, 3, 1 };
+
+            for(int i = 0; i < input.Length; i++) {
+                Console.Write(input[i]+" ");
+            }
+            Console.WriteLine("Shortest path = ");
+            Console.WriteLine(demo.jumpgame2(input));
         }
     }
 }
