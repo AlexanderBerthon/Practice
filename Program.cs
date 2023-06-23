1bd753d3320fcf4127947fdd2b63bd3da3d93453
Alex Berthon <Alexjberthon@gmail.com>
2023-06-22 17:15:08 -0700

    two sum problem

--- Program.cs
+++ Program.cs
@@ -7,12 +7,21 @@ using System.Threading.Tasks;
 
 
 namespace Practice {
-    internal class Program {
+    public class Program {
         static void Main(string[] args) {
-            StringProblems demo = new StringProblems();
+            TwoSum test = new TwoSum();
+            int[] nums = { 4, 3, 0 };
+            List<int> list = new List<int>();
+            list.Add(4);
+            list.Add(3);
+            list.Add(0);
+            Console.WriteLine("nums length: "+nums.Length);
+            Console.WriteLine("nums count: "+nums.Count());
+            Console.WriteLine("list count: "+list.Count);
 
-            demo.intToRoman(19);
+            int target = 3;
 
+            Console.WriteLine(test.solve(nums, target));
         }
     }
 }
